7ba7b897b60788d3590918ee1bf502c6a9e6fdd7
andreyromanov <andreygorogogo@gmail.com>
2020-02-13 12:13:09 +0200

    Add items

---
+++ index.js
@@ -0,0 +1,115 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+//set env
+process.env.NODE_ENV = 'production';
+
+let mainWindow;
+
+app.on('ready', function () {
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function () {
+        app.quit();
+    })
+
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+    Menu.setApplicationMenu(mainMenu);
+});
+
+//Handle add windoe function
+
+function createAddWindow() {
+
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add Item',
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    addWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'addWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    addWindow.on('close', function () {
+        addWindow = null;
+    });
+}
+
+//catch item:add
+ipcMain.on('item:add', function (e, item) {
+    console.log(item);
+    mainWindow.webContents.send('item:add', item);
+    addWindow.close();
+});
+
+//menu
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Add Item',
+                click(){
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Clear Items',
+                click(){
+                    mainWindow.webContents.send('item:clear');
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+//if macOS
+if(process.platform == 'darwin'){
+    mainMenuTemplate.unshift({});
+}
+
+//add developer tool action
+if (process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Developer',
+        submenu: [
+            {
+                label: 'Toggle Dev Tools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
